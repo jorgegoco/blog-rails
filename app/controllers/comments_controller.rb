171357e6048c93fd68469cb15a6a0bbfcbc1dc70
jorgegoco <jorgegoco70@gmail.com>
2023-06-10 23:04:16 +0200

    Fix rubocop offenses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,28 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: [:create, :destroy]
+  before_action :set_post, only: %i[create destroy]
 
   def create
     @comment = @post.comments.create(comment_params.merge(user: current_user))
 
     if @comment.save
-      flash[:notice] = "Comment was successfully created."
-      redirect_to post_path(@post)
+      flash[:notice] = 'Comment was successfully created.'
     else
-      flash[:alert] = "Comment was not created."
-      redirect_to post_path(@post)
-    end  
+      flash[:alert] = 'Comment was not created.'
+    end
+
+    redirect_to post_path(@post)
   end
 
-  def destroy 
+  def destroy
     @comment = @post.comments.find(params[:id])
     if @comment.destroy
-      flash[:notice] = "Comment was successfully deleted."
-      redirect_to post_path(@post)
+      flash[:notice] = 'Comment was successfully deleted.'
     else
-      flash[:alert] = "Comment was not deleted."
-      redirect_to post_path(@post)
+      flash[:alert] = 'Comment was not deleted.'
     end
+
+    redirect_to post_path(@post)
   end
 
   def update
@@ -30,22 +30,20 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to post_path(@post), notice: "Comment was successfully updated." }
+        format.html { redirect_to post_path(@post), notice: 'Comment was successfully updated.' }
       else
-        format.html { redirect_to post_path(@post), alert: "Comment was not updated." }
+        format.html { redirect_to post_path(@post), alert: 'Comment was not updated.' }
       end
     end
   end
 
   private
 
-  def set_post  
+  def set_post
     @post = Post.find(params[:post_id])
   end
 
   def comment_params
     params.require(:comment).permit(:body)
   end
-
-
 end
