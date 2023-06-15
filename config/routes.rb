4659f8fac85dadd9de47af2667c81d6750990059
jorgegoco <jorgegoco70@gmail.com>
2023-06-15 12:36:54 +0200

    Add posts data to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get 'admin/posts'
     get 'admin/comments'
     get 'admin/users'
-    get 'admin/show_post'
+    get 'admin/show_post/:id', to: 'admin#show_post', as: 'admin_post'
     get 'search', to: 'search#index'
     get 'users/profile'
   end
