9a3472ed08736f5975f5a99846f98ffc24ed1ee4
jorgegoco <jorgegoco70@gmail.com>
2023-06-19 17:48:13 +0200

    Fix post admin n+1 issues

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -14,7 +14,7 @@ class AdminController < ApplicationController
   end
 
   def show_post
-    @post = Post.includes(:user, :comments).find(params[:id])
+    @post = Post.includes(:user, comments: { user: {}, rich_text_body: {} }).find(params[:id])
   end
 
   def show_comment
