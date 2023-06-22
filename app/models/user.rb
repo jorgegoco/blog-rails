b7f06417147ba4d439f2be5f5ab4f73f7fe86989
jorgegoco <jorgegoco70@gmail.com>
2023-06-22 19:15:32 +0200

    Setup user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,25 @@ class User < ApplicationRecord
 
   has_many :notifications, as: :recipient, dependent: :destroy # if user is deleted, delete all associated notifications
 
+  has_one :address, dependent: :destroy, inverse_of: :user, autosave: true 
+
   enum role: %i[user admin]
   after_initialize :set_default_role, if: :new_record?
+
+  cattr_accessor :form_steps do
+    %w[sign_up set_name set_address find_users]
+  end
+
+  attr_accessor :form_step
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
 
+  accepts_nested_attributes_for :address, allow_destroy: true
+
   def self.ransackable_attributes(_ = nil)
-    %w[email name]
+    %w[email first_name last_name]
   end
 
   private
