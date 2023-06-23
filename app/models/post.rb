f9729e3f447cc93e9b6ca31590993f7e0f6197d0
jorgegoco <jorgegoco70@gmail.com>
2023-06-23 20:52:46 +0200

    Add associations to category, and data to seeds file

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true
 
+  belongs_to :category
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 
