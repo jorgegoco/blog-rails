7348f82ad9c21fed927f4e5fb8d6ea95f50a7196
jorgegoco <jorgegoco70@gmail.com>
2023-06-24 22:25:49 +0200

    Fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,18 @@ class ApplicationController < ActionController::Base
     @query = Post.ransack(params[:q])
   end
 
+  # rubocop:disable Naming/PredicateName
   def is_admin!
     redirect_to root_path, alert: "You don't have permission to do that." unless current_user&.admin?
   end
+  # rubocop:enable Naming/PredicateName
 
   private
-  
+
   def set_categories
     @nav_categories = Category.where(display_in_nav: true).order(:name)
   end
-  
+
   def set_notifications
     notifications = Notification.includes(:recipient).where(recipient: current_user).newest_first.limit(9)
     @unread = notifications.unread
